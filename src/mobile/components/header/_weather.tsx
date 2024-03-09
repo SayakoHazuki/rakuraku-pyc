294271d715d9c48f495dee9905b9e94181cbde4d
Sayako Hazuki <amf.kano@gmail.com>
2024-03-10 02:32:10 +0800

    Functional temperature display

--- src/mobile/components/header/_weather.tsx
+++ src/mobile/components/header/_weather.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 export default function MHeader_Weather() {
+  const [temp, setTemp] = useState(0);
+  useEffect(() => {
+    fetch(
+      "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc"
+    ).then((res) => {
+      res.json().then((data) => {
+        setTemp(data.temperature.data[1].value);
+      });
+    });
+  }, []);
+
   return (
     <div className={styles.headerWeather}>
       <div className={styles.temp}>
-        <span>27</span>°C
+        <span>{temp}</span>°C
       </div>
     </div>
   );
